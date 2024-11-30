281f93b0015044529c4096a7b24b7afc7afb82ee
gustaVASSIO <guguzim.anacleto@gmail.com>
2024-11-29 23:14:19 -0300

    Update log

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import { log } from "./log.mjs";
 
 export const handler = async (event) => {
     log("Log de execução", JSON.stringify(event))
+    log("Publicando alterção na funcion via GH Actions")
     
     return {
       statusCode: 200,
